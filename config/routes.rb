071fe825b16ce56b399f6216bbea0c716be9bea8
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-04-16 22:44:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  root to: "foods#index"
-  resources :foods, only: :index
-=======
   root to: "users#index"
   resources :users, only: :index
   
@@ -11,5 +7,4 @@ Rails.application.routes.draw do
       get 'calculation', to: 'records#calculation'
     end
   end
->>>>>>> Stashed changes
 end
