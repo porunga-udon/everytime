57bd4354813de2be5065b6c4c1f4a9db7c952284
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-04-16 22:39:12 +0900

    add_records_calculation_path
    recordsのcalculationを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   root to: "foods#index"
   resources :foods, only: :index
+=======
+  root to: "users#index"
+  resources :users, only: :index
+  
+  resources :records, only: :calculation do
+    collection do
+      get 'calculation', to: 'records#calculation'
+    end
+  end
+>>>>>>> Stashed changes
 end
