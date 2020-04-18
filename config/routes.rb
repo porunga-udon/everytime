ace2cd22025eadce02ddf5b95d1cfc094b7a5304
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-04-18 17:23:32 +0900

    add_foods_index_route
    ビュー作成のため、foods#indexを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
   resources :users, only: :index
+
+  resources :foods, only: :index
   
   resources :records, only: :calculation do
     collection do
