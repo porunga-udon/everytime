86e6292795fc5f172d9843b3b476e6f71a84bc7e
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-04-23 16:17:32 +0900

    @foodsの追加と不要な記述の削除

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,10 +2,10 @@ class FoodsController < ApplicationController
   before_action :create_hash
 
   def index
+    @foods = Food.all
   end
 
   def create
-    # binding.pry
     @food = Food.new(food_params)
     @food.save
     redirect_to foods_path
@@ -18,9 +18,7 @@ class FoodsController < ApplicationController
 
   def create_hash
     @meals = ["朝食","昼食","夕食","間食"]
-    @foods = ["チキンラーメン","はなまるうどん","骨付きステーキ"]
     @nutrition = [{ja:"カロリー", eng:"kcal", column:"calorie"},{ja:"炭水化物", eng:"g", column:"carbo"},{ja:"脂肪", eng:"g", column:"fat"},{ja:"タンパク質", eng:"g", column:"protein"}]
-    @data = [100,80,60,40]
   end
 
 end
