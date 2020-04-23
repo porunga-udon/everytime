43a436374da718f6f72d645417f1d3fe4ad6e817
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-04-23 10:36:41 +0900

    新しいフードを追加する機能

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,14 +5,21 @@ class FoodsController < ApplicationController
   end
 
   def create
-    
+    # binding.pry
+    @food = Food.new(food_params)
+    @food.save
+    redirect_to foods_path
   end
 
   private
+  def food_params
+    params.permit(:name, :quantity, :calorie, :carbo, :fat, :protein)
+  end
+
   def create_hash
     @meals = ["朝食","昼食","夕食","間食"]
     @foods = ["チキンラーメン","はなまるうどん","骨付きステーキ"]
-    @nutrition = [{ja:"カロリー", eng:"kcal"},{ja:"炭水化物", eng:"g"},{ja:"脂肪", eng:"g"},{ja:"タンパク質", eng:"g"}]
+    @nutrition = [{ja:"カロリー", eng:"kcal", column:"calorie"},{ja:"炭水化物", eng:"g", column:"carbo"},{ja:"脂肪", eng:"g", column:"fat"},{ja:"タンパク質", eng:"g", column:"protein"}]
     @data = [100,80,60,40]
   end
 
