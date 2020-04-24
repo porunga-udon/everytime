4c2eedfe4d9891c67913745cfe513cd219d0b6eb
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-04-24 18:44:27 +0900

    追加ボタンクリックでデータを取得する

--- app/assets/javascripts/add_food.js
+++ app/assets/javascripts/add_food.js
@@ -1,17 +1,33 @@
 $(function() {
 
+  // function buildHTML(message){
+  //   let html = `
+  //   `
+  // }
+
   // フード追加モーダル
   $('.add').click(function(e) {
     var addId = this.id
     console.log(addId);
     $('.modal-overlay').fadeIn("fast");
     $('.modal_foods').fadeIn("fast");
-
-    $('.fa-plus-circle').click(function(){
-      console.log('okok');
-      $('.modal-overlay').fadeOut("fast");
-      $('.modal_foods').fadeOut("fast");
-    })
+  })
+  
+  // 追加ボタンクリック
+  $('.fa-plus-circle').click(function(e){
+    e.preventDefault();
+    let food_id = this.id
+    let nutrition = document.getElementById('nutrition' + food_id).children
+    let data = {
+      name   :document.getElementById('name' + food_id).innerHTML,
+      calorie:nutrition[0].textContent,
+      carbo  :nutrition[1].textContent,
+      fat    :nutrition[2].textContent,
+      protein:nutrition[3].textContent,
+    }
+    let food_list = buildHTML(data);
+    $('.modal-overlay').fadeOut("fast");
+    $('.modal_foods').fadeOut("fast");
   })
 
   // モーダルを閉じる
