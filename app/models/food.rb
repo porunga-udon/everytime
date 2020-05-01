075969a4d88719162682780db58de673459a24c5
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-05-01 22:04:43 +0900

    栄養のフォームが空の場合には０とする

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,7 @@ class Food < ApplicationRecord
   validates :quantity,         presence: true
   validates :one_of_nutrition, presence: true, format: {with: /\A[0-9]+\z/ }, inclusion: 0..999
 
+  # インクリメンタルサーチ
   def self.search(input)
     if input == ""
       Food.all
@@ -11,10 +12,16 @@ class Food < ApplicationRecord
     end
   end
 
+  def column_check
+    self.calorie = 0 if self.calorie.blank?
+    self.carbo   = 0 if self.carbo.blank?
+    self.fat     = 0 if self.fat.blank?
+    self.protein = 0 if self.protein.blank?
+  end
+
   private
     def one_of_nutrition
       calorie.presence or carbo.presence or fat.presence or protein.presence
     end
-
 
 end
