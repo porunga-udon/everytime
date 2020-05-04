458e4475d5a1b2d686a5b15c1d7e0d27f6215016
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-05-04 17:56:57 +0900

    user_datasに変更

--- test/controllers/user_datas_controller_test.rb
+++ test/controllers/user_datas_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DatasControllerTest < ActionDispatch::IntegrationTest
+class UserDatasControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
