a262809e581e41b655b2054044cde4e29b94ef8d
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-05-16 19:00:37 +0900

    修正

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,5 @@
 class Meal < ActiveHash::Base
   self.data = [
-    {id:1, meal:"朝食"}, {id:2, meal:"昼食"}, {id:3, meal:"夕食"}, {id:4, meal:"間食"},
+    {id:1, index:"朝食"}, {id:2, index:"昼食"}, {id:3, index:"夕食"}, {id:4, index:"間食"},
   ]
 end
