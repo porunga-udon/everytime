f4cee326c8c37561123df229a8aba45ad01e2e73
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-05-16 11:27:35 +0900

    アソシエーション・バリデーションを設定

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,11 @@
 class Diary < ApplicationRecord
+  belongs_to :user
+  has_many   :foods, through: :diary_foods
+
+  validates :registration_date,        present:true
+  validates :meal_id,                  present:true
+  validates :user_id,                  present:true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :meal
 end
