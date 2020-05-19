3d4f28e1c2bafe50a723dcdab73a664ea9632337
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-05-19 20:13:52 +0900

    マイグレーションファイル変更

--- db/migrate/20200516022856_create_diary_foods.rb
+++ db/migrate/20200516022856_create_diary_foods.rb
@@ -1,8 +1,8 @@
 class CreateDiaryFoods < ActiveRecord::Migration[5.2]
   def change
     create_table :diary_foods do |t|
-      t.references :diary, null: false, foreign_key: true
-      t.references :food,  null: false, foreign_key: true
+      t.references :diary, foreign_key: true
+      t.references :food,  foreign_key: true
       t.timestamps
     end
   end
