a97048e8251494351fc0282f0f2e846f83a5363e
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-05-20 11:51:21 +0900

    servingのactive_hashを作成

---
+++ app/models/serving.rb
@@ -0,0 +1,5 @@
+class Serving < ActiveHash::Base
+  self.data = [
+    {id:1, index:1}, {id:2, index:2}, {id:3, index:3}, {id:4, index:4}, {id:5, index:5}, {id:6, index:6}, {id:7, index:7}, {id:8, index:8}, {id:9, index:9}
+  ]
+end
