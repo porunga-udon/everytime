10238fc48492474c2c2a067c5b1c8257f1836a01
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-06-22 11:38:21 +0900

    ダイアリーのcsv出力機能を実装

--- app/assets/javascripts/diary_index.js
+++ app/assets/javascripts/diary_index.js
@@ -182,11 +182,13 @@ $(function() {
     $('.diary_main__all__data--num').text(0);
   }
 
-  // 出力モーダルの表示-------------------------------
+  // csv出力モーダルの表示-------------------------------
   $('.add_modal').click(function() {
     modalResize();
     $('.modal-overlay').fadeIn("fast");
     $('.modal_diary').fadeIn("fast");
+
+    // 「出力する」ボタンを押した場合の処理
     $('#create_csv').click(function() {
       let sy = $('#start_year   option:selected').text();
       let sm = ("00" + $('#start_month  option:selected').text()).slice(-2);
@@ -194,7 +196,7 @@ $(function() {
       let fy = $('#finish_year  option:selected').text();
       let fm = ("00" + $('#finish_month option:selected').text()).slice(-2);
       let fd = ("00" + $('#finish_day   option:selected').text()).slice(-2);
-
+      // 開始日と終了日を作成
       let start_date   = sy + sm + sd
       let finish_date  = fy + fm + fd
       $.ajax ({
@@ -208,15 +210,105 @@ $(function() {
       })
       // 成功
       .done(function(data) {
-        console.log(data);
+
         console.log(data[0]);
-        console.log(data[0][0])
+
+        let CsvArray  = []
+        let MealName  = [['朝食'],['昼食'],['夕食'],['間食']]
+        let Nutrition = ["", "量","カロリー", "炭水化物", "脂肪", "タンパク質"]
+
+        // csv出力用の配列を作る処理
+        for (let i = 0; i < data.length; i++) {
+          let DayData = data[i];
+          let DiaryDay = DayData.shift();
+          // 日付を取り出し配列型に処理する
+          let ProcessedDay = Object.keys(DiaryDay).map(function (key) {return DiaryDay[key]})
+          // 開始日かそれ以外かで処理を変える
+          if (CsvArray.length === 0) {
+            CsvArray.push(ProcessedDay,Nutrition);
+          } else {
+            CsvArray.push(Nutrition,[],ProcessedDay,Nutrition);
+          }
+
+          let Calorie = []
+          let Carbo   = []
+          let Fat     = []
+          let Protein = []
+
+          for (let m = 0; m < DayData.length; m++) {
+            let MealData = DayData[m];
+            // 食事の登録の有無で処理を変える
+            if (MealData.length === 0) {
+              // 朝食かどうかで処理を変える
+              if (MealName[m] === MealName[0]) {
+                CsvArray.push(MealName[m],['なし'])
+              } else {
+                CsvArray.push([],MealName[m],['なし'])
+              }
+            } else {
+              // 朝食かどうかで処理を変える
+              if (MealName[m] === MealName[0]) {
+                CsvArray.push(MealName[m])
+              } else {
+                CsvArray.push([],MealName[m])
+              }
+              
+              // 食べ物のデータを配列化する
+              for (let f = 0; f < MealData.length; f++) {
+                let FoodData = MealData[f];
+                let ProcessedFood = Object.keys(FoodData).map(function (key) {return FoodData[key]})
+                CsvArray.push(ProcessedFood);
+                // 栄養の合計値を出すため配列を作成
+                Calorie.push(ProcessedFood[2]);
+                Carbo.push(ProcessedFood[3]);
+                Fat.push(ProcessedFood[4]);
+                Protein.push(ProcessedFood[5]);
+              }
+            }
+          }
+
+          // 日毎の合計値を配列に追加
+          let TotalCalorie = 0
+          let TotalCarbo   = 0
+          let TotalFat     = 0
+          let TotalProtein = 0
+          for (let v = 0; v < Calorie.length; v++) {
+            TotalCalorie += Calorie[v]
+            TotalCarbo   += Carbo[v]
+            TotalFat     += Fat[v]
+            TotalProtein += Protein[v]
+          }
+          CsvArray.push([], ['合計値','', TotalCalorie, TotalCarbo, TotalFat, TotalProtein]);
+        }
+
+        console.log(CsvArray);
+
+      // BOM の用意（文字化け対策）
+      let bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+
+      // CSV データの用意
+      let csv_data = CsvArray.map(function(array){return array.join(',')}).join('\r\n');
+  
+      let blob = new Blob([bom, csv_data], { type: 'text/csv' });
+  
+      let url = window.URL.createObjectURL(blob);
+  
+      let a = document.getElementById('downloader');
+  
+      // a.download = date + '.csv';       //日付+.csv
+      a.download = 'ポルンガ.csv';
+  
+      a.href = url;
+  
+      // ダウンロードリンクをクリックする
+      $('#downloader')[0].click();
       })
       // 失敗
       .fail(function() {
         console.log('miss');
       })
 
+      // 出力後モーダルを閉じる
       $('.modal-overlay').fadeOut("fast");
       $('.modal_diary').fadeOut("fast");
       $("#create_csv").off();
